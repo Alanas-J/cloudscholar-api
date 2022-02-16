f9c250031d10e42c2962614f1d7cc3d13f0ede27
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-02-16 20:25:10 +0000

    mongoDB relationships and nested object data population logic

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,35 +1,109 @@
 const e = require('express');
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 
 // Get Products
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET Request to /orders.'
-    });
+
+    Order.find()
+        .select('_id product quantity')
+        .populate('product', '_id name price') // used to parse ID into object data
+        .exec()
+        .then( result => {
+            res.status(200).json({
+                count: result.length,
+                orders: result.map(doc => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity
+                    }
+                })
+            });
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
+    Product.findById(req.body.productID)
+        .then(product => {
 
-    const order = {
-        productID: req.body.productID,
-        quantity: req.body.quantity
-    }
+            if(!product){
+                return res.status(404).json({
+                    message: 'No product found with the id:' + req.body.productID
+                })
+            }
 
-    res.status(201).json({
-        message: 'Handling POST Request to /orders.',
-        order: order
-    });
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productID
+            });
+        
+            return order.save();
+        })
+        .then( result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        }); 
+
 });
 
 
 router.get('/:orderID', (req, res, next) => {
-    const id = req.params.orderID;
+    Order.findById(req.params.orderID)
+        .populate('product')
+        .exec()
+        .then( order => {
 
-    res.status(200).json({
-        message: `Got order ID: ${id}`
-    });
+            if(!order){
+                return res.status(404).json({
+                    message: 'Order not found'
+                });
+            }
+
+            res.status(200).json({
+                order
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        }); 
 });
+
+router.delete('/:orderID', (req, res, next) => {
+    Order.remove(req.params.orderID)
+        .exec()
+        .then( result => {
+            res.status(200).json({
+                result
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        }); 
+});
+
 
 module.exports = router;
