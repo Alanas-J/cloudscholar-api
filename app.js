d0a0630da5c58e8c85c3372fe309538c8b2ba7c8
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-02-16 10:35:34 +0000

    logging and error handling

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); // middleware for request logging
 
 const productRoutes = require('./api/routes/products');
-const orderRouters = require('./api/routes/orders');
+const orderRoutes = require('./api/routes/orders');
+
+// middleware
+app.use(morgan('dev'));
 
 
+// Route Handing =============================================
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// Catches all other routes
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+
+    next(error);
+});
+
+
+// Catches errors where thrown. ============================
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
