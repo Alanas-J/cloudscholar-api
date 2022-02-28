a56f1e50cb8b96e549b95231a2d2ef95cb3629f9
Alanas-J <alanasjakubauskas@gmail.com>
2022-02-28 14:34:54 +0000

    updated sql file runs now

--- database_scripts/db_init.sql
+++ database_scripts/db_init.sql
@@ -1,24 +1,23 @@
 -- Is used to comment
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS subject;
-DROP TABLE IF EXISTS class;
-DROP TABLE IF EXISTS task;
-DROP TABLE IF EXISTS shortcut_link;
+DROP TABLE IF EXISTS cloudscholar_class;
+DROP TABLE IF EXISTS cloudscholar_task;
+DROP TABLE IF EXISTS cloudscholar_shortcut_link;
+DROP TABLE IF EXISTS cloudscholar_subject;
+DROP TABLE IF EXISTS cloudscholar_user;
 
 
-CREATE TABLE user (
+CREATE TABLE cloudscholar_user (
     user_id SERIAL PRIMARY KEY,
 
     email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
 );
 
 
-CREATE TABLE subject (
+CREATE TABLE cloudscholar_subject (
     subject_id SERIAL PRIMARY KEY,
-    user_id INT,
+    user_id INT NOT NULL,
 
-    user_id int NOT NULL,
     day int,
     name VARCHAR(255) NOT NULL,
     colour VARCHAR(255),
@@ -26,11 +25,11 @@ CREATE TABLE subject (
     end_date DATE NOT NULL,
     description VARCHAR(512),
 
-    CONSTRAINT fk_user FOREIGN KEY(user_id) REFFERENCES user(user_id)
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES cloudscholar_user(user_id)
 );
 
 
-CREATE TABLE class (
+CREATE TABLE cloudscholar_class (
     class_id SERIAL PRIMARY KEY,
     subject_id INT NOT NULL,
 
@@ -41,11 +40,11 @@ CREATE TABLE class (
     end_time TIME,
     description VARCHAR(512),
 
-    CONSTRAINT fk_subject FOREIGN KEY(subject_id) REFFERENCES user(subject_id)
+    CONSTRAINT fk_subject FOREIGN KEY(subject_id) REFERENCES cloudscholar_subject(subject_id)
 );
 
 
-CREATE TABLE task (
+CREATE TABLE cloudscholar_task (
     task_id SERIAL PRIMARY KEY,
     subject_id INT,
 
@@ -53,16 +52,18 @@ CREATE TABLE task (
     due_datetime TIMESTAMP,
     description VARCHAR(512),
 
-    CONSTRAINT fk_subject FOREIGN KEY(subject_id) REFFERENCES user(subject_id)
+    CONSTRAINT fk_subject FOREIGN KEY(subject_id) REFERENCES cloudscholar_subject(subject_id)
 );
 
 
-CREATE TABLE shortcut_link (
+CREATE TABLE cloudscholar_shortcut_link (
     shortcut_link_id SERIAL PRIMARY KEY,
     user_id INT,
 
     name VARCHAR(255),
     url VARCHAR(255),
 
-    CONSTRAINT fk_user FOREIGN KEY(user_id) REFFERENCES user(user_id)
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES cloudscholar_user(user_id)
 );
+
+-- commit;
