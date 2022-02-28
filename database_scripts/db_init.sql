2e0c9d49ed681389281bcf6c97a5360e24d3e8c8
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-02-28 11:20:33 +0000

    extra polish on sql schema

--- database_scripts/db_init.sql
+++ database_scripts/db_init.sql
@@ -1,5 +1,4 @@
 -- Is used to comment
-
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS subject;
 DROP TABLE IF EXISTS class;
@@ -21,11 +20,11 @@ CREATE TABLE subject (
 
     user_id int NOT NULL,
     day int,
-    name varchar(255) NOT NULL,
-    colour varchar(255),
-    start_date varchar(255) NOT NULL,
-    end_date varchar(255) NOT NULL,
-    description varchar(512),
+    name VARCHAR(255) NOT NULL,
+    colour VARCHAR(255),
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    description VARCHAR(512),
 
     CONSTRAINT fk_user FOREIGN KEY(user_id) REFFERENCES user(user_id)
 );
@@ -38,8 +37,8 @@ CREATE TABLE class (
     day INT,
     type VARCHAR(255),
     location VARCHAR(255),
-    start_time VARCHAR(255),
-    end_time VARCHAR(255),
+    start_time TIME,
+    end_time TIME,
     description VARCHAR(512),
 
     CONSTRAINT fk_subject FOREIGN KEY(subject_id) REFFERENCES user(subject_id)
@@ -51,7 +50,7 @@ CREATE TABLE task (
     subject_id INT,
 
     name VARCHAR(255),
-    due_datetime VARCHAR(255),
+    due_datetime TIMESTAMP,
     description VARCHAR(512),
 
     CONSTRAINT fk_subject FOREIGN KEY(subject_id) REFFERENCES user(subject_id)
