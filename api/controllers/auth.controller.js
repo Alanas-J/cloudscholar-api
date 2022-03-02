6b61f8adaf7877dd99aa8babb8e782c6c8ee8f1e
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-03-02 18:26:09 +0000

    login auth token working

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const db = require("../models");
 const Op = db.Sequelize.Op;
 
@@ -6,6 +7,48 @@ const User = db.users;
 
 
 exports.login = (req, res, next) => {
+    // Validate request.
+    if (!req.body.email || !req.body.password) {
+        res.status(400).send({
+          message: "'email' and 'password' fields expected."
+        });
+        return;
+    }
+
+    User.findOne({where: { email: req.body.email }})
+        .then(user => {
+            if (user) {
+                bcrypt.compare(req.body.password, user.password, (err, result) => {
+                    if(err || !result){
+                        return res.status(401).json({
+                            message: 'Login failed.'
+                        });
+                    }
+
+                    if (result) {
+                        const token = jwt.sign(
+                          {
+                            email: user.email,
+                            userId: user.id
+                          },
+                          process.env.JWT_KEY,
+                          {
+                              expiresIn: "1h"
+                          }
+                        );
+                        return res.status(200).json({
+                          message: "Login success!",
+                          token: token
+                        });
+                      }
+                    
+                });
+            } else{
+                res.status(401).json({
+                    message: 'Login failed.'
+                }); 
+            }
+        });
 
 }
 
