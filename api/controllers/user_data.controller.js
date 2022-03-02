844c3d3ccd2c88e535ca127ccd8d09ffba00d897
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-03-02 19:21:40 +0000

    able to fetch nested associations

--- api/controllers/user_data.controller.js
+++ api/controllers/user_data.controller.js
@@ -1,10 +1,44 @@
+const db = require("../models");
+const User = db.users;
+const Subjects = db.subjects;
+const Tasks = db.tasks;
+const Classes = db.classes;
+const ShortcutLinks= db.shortcut_links;
 
-const checkAuth = require('../middleware/check-auth');
+exports.getById = (req, res, next) => {
 
-exports.getById = (checkAuth, req, res, next) => {
+    console.log(req.params.id);
 
-}
+    User.findByPk(req.params.id, { include: [
+            {
+                association: "subjects",
+                include: ["classes", "tasks"]
+            },
+            {
+                association: "shortcut_links"
+            }]
+        })
+        .then(data => {
 
-exports.updateById = (checkAuth, req, res, next) => {
+            console.log(data);
 
-}
+            if (data) {
+                console.log(data);
+                res.send(data);
+            } else {
+            res.status(404).send({
+                message: `Cannot find Tutorial with id=${req.params.id}.`
+            });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+            message: "Error retrieving Tutorial with id=" + req.params.id
+            });
+        });
+
+};
+
+exports.updateById = (req, res, next) => {
+
+};
--- api/routes/user_data.routes.js
+++ api/routes/user_data.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const user_data = require("../controllers/user_data.controller.js");
 
-router.get('/:user_id', user_data.getById);
+router.get('/:id', user_data.getById);
 
-router.post('/:user_id', user_data.updateById);
+router.post('/:id', user_data.updateById);
 
 module.exports = router;
