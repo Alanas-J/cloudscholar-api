447ded4ff6181e1454973eade2923232934f5744
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-03-05 21:53:54 +0000

    stopping progress for today, model and login logic written, need refresh and fix expiry date bug

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const db = require("../models");
 
 const User = db.users;
+const RefreshToken = db.refresh_tokens;
 
 
 exports.login = (req, res, next) => {
@@ -17,7 +18,7 @@ exports.login = (req, res, next) => {
     User.findOne({where: { email: req.body.email }})
         .then(user => {
             if (user) {
-                bcrypt.compare(req.body.password, user.password, (err, result) => {
+                bcrypt.compare(req.body.password, user.password, async (err, result) => {
                     if(err || !result){
                         return res.status(401).json({
                             message: 'Login failed, email or pasword is incorrect.'
@@ -26,20 +27,24 @@ exports.login = (req, res, next) => {
 
                     if (result) {
                         const token = jwt.sign(
-                          {
-                            email: user.email,
-                            userId: user.id
-                          },
-                          process.env.JWT_KEY,
-                          {
-                              expiresIn: "1h"
-                          }
+                            {
+                                email: user.email,
+                                userId: user.id
+                            },
+                            process.env.JWT_KEY,
+                            {
+                                 expiresIn: "1h"
+                            }
                         );
+
+                        const refresh_token = await RefreshToken.createToken(user);
+
                         return res.status(200).json({
-                          message: "Login success!",
-                          token: token
+                             message: "Login success!",
+                             token: token,
+                             refresh_token: refresh_token
                         });
-                      }
+                    }
                     
                 });
             } else{
@@ -47,6 +52,12 @@ exports.login = (req, res, next) => {
                     message: 'Login failed, email or pasword is incorrect.'
                 }); 
             }
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'Internal Server Error on Login...',
+                error: error
+            }); 
         });
 
 }
