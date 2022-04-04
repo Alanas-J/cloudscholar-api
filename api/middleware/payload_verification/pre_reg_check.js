c2821b06a6461b1e2e82d838bf6ea42721553aae
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-04-04 21:09:11 +0100

    email regex validation added

--- api/middleware/payload_verification/pre_reg_check.js
+++ api/middleware/payload_verification/pre_reg_check.js
@@ -3,7 +3,6 @@ const User = db.users;
 
 module.exports = (req, res, next) => {
     
-    // Validate body contents (will need regex later).
     if (!req.body.email || !req.body.password) {
         res.status(400).send({
           message: "Email and password fields expected."
@@ -11,9 +10,16 @@ module.exports = (req, res, next) => {
         return;
     }
 
-    // Check if email is unique.
+    if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(req.body.email)){
+        res.status(400).send({
+            message: "Invalid email."
+          });
+          return;
+    }
+
     User.findOne({where: { email: req.body.email }})
         .then(user => {
+            
             if (user) {
                 res.status(400).send({
                     message: "This email is already in use."
