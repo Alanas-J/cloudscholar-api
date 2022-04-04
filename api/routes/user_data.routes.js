978a8498e37f19c5f30468875b58f5c7eda426d8
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-04-04 19:32:21 +0100

    removal of old testing routes

--- api/routes/user_data.routes.js
+++ api/routes/user_data.routes.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const user_data = require("../controllers/user_data.controller.js");
 const process_auth_jwt = require('../middleware/authentication/process_auth_jwt.js');
 
-// Uses JWT to identify which user to fetch.
 router.get('', process_auth_jwt, user_data.get);
-router.post('', process_auth_jwt, user_data.update);
 
-// Routes for development
-router.get('/:id', user_data.getById);
-router.post('/:id', user_data.updateById);
+router.post('', process_auth_jwt, user_data.update);
 
 
 module.exports = router;
