26f06b92f679f3f91d5da46cb71f1f60277bf008
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-04-04 12:49:19 +0100

    CORS updated to wildcard

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Orgin, X-RequestedWith, Content-Type, Accept, Authorization');
 
     if(req.method === 'OPTIONS'){ 
