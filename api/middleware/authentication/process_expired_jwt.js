cbce72f9f34c53979893732d54d5a03d3b1895a7
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-04-04 22:05:30 +0100

    Update process_expired_jwt.js

--- api/middleware/authentication/process_expired_jwt.js
+++ api/middleware/authentication/process_expired_jwt.js
@@ -1,26 +1,6 @@
 const jwt = require('jsonwebtoken');
 const env = require(`../../../config/env.${process.env.NODE_ENV}.config`);
 
-// Does the auth header have a JWT?
-// if NOT - no token found error.
-
-// Is the JWT expired?
-// if NOT - token active error.
-
-// Is there a refresh token attached?
-// if NOT - no token error.
-
-// Need to implement check if JWT is linked to the refresh token.
-// Send onto next.
-
-// Else token and JWT not related
-
-// Alternative: process expired jwt
-// Just check if a JWT is present and expired.
-// else error.
-
-// Controller checks if the token contents are linked to the refresh.
-
 module.exports = (req, res, next) => {
 
     if(!req.headers.authorization){
