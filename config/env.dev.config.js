ab3ec7b92d7ed339ae9cc0db7f81fa6bd59c82cd
Alanas-J <34601418+Alanas-J@users.noreply.github.com>
2022-04-08 22:42:16 +0100

    configuring dev

---
+++ config/env.dev.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    JWT: {
+        KEY: "secret_key",
+        EXPIRATION: '1h',
+        REFRESH_TOKEN: {
+            EXPIRATION: 100000
+        } 
+    },
+        
+    DATABASE: {
+        HOST: "cloudscholar-dev-db.cqjsibxccdnt.eu-west-1.rds.amazonaws.com",
+        USER: "postgres",
+        PASSWORD: "cs-password",
+        DB: "postgres",
+        dialect: "postgres",
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+};
